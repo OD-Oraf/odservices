05bde71e7d80c7455ca408aeb5bd0aba5551449f
od <poseidonog@gmail.com>
2023-05-30 23:04:58 -0400

    add TODO for async queue

--- customer/src/main/java/com/odoraf/customer/CustomerService.java
+++ customer/src/main/java/com/odoraf/customer/CustomerService.java
@@ -34,7 +34,7 @@ public class CustomerService {
             throw new IllegalStateException("fraudster");
         }
 
-
+        // TODO: Make async, add to queue
         notificationClient.sendNotification(
                 new NotificationRequest(
                         customer.getId(),
