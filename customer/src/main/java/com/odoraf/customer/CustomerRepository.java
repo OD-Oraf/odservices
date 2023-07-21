cdbfea67b6061ff5864ee0c6c47c32a88022c914
od <poseidonog@gmail.com>
2023-07-21 10:32:22 -0400

    add simple email validation, min length for first name and lastname

--- customer/src/main/java/com/odoraf/customer/CustomerRepository.java
+++ customer/src/main/java/com/odoraf/customer/CustomerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // Interface to inherit all methods that are used to interact with DB
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+    Boolean existsByEmail(String email);
 }
