db540c21e56ff9cee3abb9ffb1620ca302d54683
od <poseidonog@gmail.com>
2023-07-20 23:40:27 -0400

    add custom response code and set hibernate to update

--- customer/src/main/java/com/odoraf/customer/CustomerController.java
+++ customer/src/main/java/com/odoraf/customer/CustomerController.java
@@ -1,7 +1,10 @@
 package com.odoraf.customer;
 
+import com.odoraf.customer.response.ResponseHandler;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +25,21 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
-        log.info("new customer registration {}", customerRegistrationRequest);
-        customerService.registerCustomer(customerRegistrationRequest);
+    public ResponseEntity<Object> registerCustomer(
+            @RequestBody CustomerRegistrationRequest customerRegistrationRequest
+    ) {
+        try {
+            log.info("new customer registration {}", customerRegistrationRequest);
+            customerService.registerCustomer(customerRegistrationRequest);
+
+            return ResponseHandler.generateResponse(
+                    "User successfully added",
+                    HttpStatus.OK,
+                    customerRegistrationRequest
+            );
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR,null);
+        }
+
     }
 }
